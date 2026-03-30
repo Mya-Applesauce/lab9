1aec265c2cf72f2bcb4411750cb54b267ace57ff
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-30 13:29:12 -0400

    set up the init method

--- player.py
+++ player.py
@@ -4,3 +4,13 @@ Ari Papke
 player class
 03/29/26
 """
+
+from coin import Coin
+
+class Player:
+    def __init__(self, name):
+        self.__name = name
+        self.__wallet = 20
+        self.__coin = Coin()
+
+    
