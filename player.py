c611fe8fc9425a0d76e3ac7061d80a6d54447a0d
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-30 15:33:53 -0400

    added the other methods

--- player.py
+++ player.py
@@ -19,16 +19,26 @@ class Player:
     def get_coin_side(self):
         return self.__coin.get_sideup()
 
-    #def win_coin(self):
+    def win_coin(self, gain):
+        self.__wallet += gain
 
-    #def lose_coin(self):
+    def lose_coin(self, loss):
+        self.__wallet -= loss
 
-    #def get_wallet(self):
+    def get_wallet(self):
+        return self.__wallet
 
-    #def get_name(self):
+    def get_name(self):
+        return self.__name
 
-coin_toss = Player("n")
+#coin_toss = Player("n")
 
-for ii in range(10):
-    coin_toss.toss_coin()
-    print(f"after toss, side up is {coin_toss.get_coin_side()}")
+#for ii in range(10):
+    #coin_toss.toss_coin()
+    #print(f"after toss, side up is {coin_toss.get_coin_side()}")
+    #if coin_toss.get_coin_side() == 0:
+        #coin_toss.win_coin(1)
+    #elif coin_toss.get_coin_side() == 1:
+        #coin_toss.lose_coin(1)
+
+#print(f"{coin_toss.get_name()}'s wallet has {coin_toss.get_wallet()} coins")
