a69f959aabcd70fd657114342c547f4258b019e7
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-29 22:44:48 -0400

    put in the class methods and tested them

--- coin.py
+++ coin.py
@@ -4,3 +4,28 @@ coin class
 03/29/26
 """
 
+import random
+
+class Coin:
+    def __init__(self):
+        self.__sideup = "Heads!"
+
+    def toss(self):
+        side = random.randrange(2)
+        if side == 0:
+            self.__sideup = "Heads!"
+        elif side == 1:
+            self.__sideup = "Tails!" 
+        else:
+            print("I don't understand ranges")
+
+    def get_sideup(self):
+        return self.__sideup
+
+coin_toss = Coin()
+
+print("the value of the coin before we toss it is", 
+      coin_toss.get_sideup())
+for ii in range(10):
+    coin_toss.toss()
+    print(f"after toss, side up is {coin_toss.get_sideup()}")
