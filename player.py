31d678bf8a8d09bc563dd4fd6c176f1686d9a6a5
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-30 15:12:14 -0400

    set up and tested the toss coin and get coin side methods

--- player.py
+++ player.py
@@ -13,4 +13,22 @@ class Player:
         self.__wallet = 20
         self.__coin = Coin()
 
-    
+    def toss_coin(self):
+        self.__coin.toss()
+
+    def get_coin_side(self):
+        return self.__coin.get_sideup()
+
+    #def win_coin(self):
+
+    #def lose_coin(self):
+
+    #def get_wallet(self):
+
+    #def get_name(self):
+
+coin_toss = Player("n")
+
+for ii in range(10):
+    coin_toss.toss_coin()
+    print(f"after toss, side up is {coin_toss.get_coin_side()}")
