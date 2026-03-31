51af7687d1fe76a05e5db026e232795ba8b84c6d
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-30 20:59:17 -0400

    got what i had so far to work better

--- Lab9_apapke-1.py
+++ Lab9_apapke-1.py
@@ -11,20 +11,23 @@ def main():
     player1 = Player("Sonic")
     player2 = Player("Tails")
 
-print(f"+++ Coin Match Madness! +++")
+    print(f"+++ Coin Match Madness! +++")
 
-active = True
+    active = True
 
-while active:
-    print(f"{main()} has {} coins.")
-    print(f"{main()} has {} coins.\n")
+    while active:
+        print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
+        print(f"{player2.get_name()} has {player2.get_wallet()} coins.\n")
     
-    keep_playing = input("Do you want to play? (y/n): ")
-    if keep_playing == "y" or keep_playing == "Y" or keep_playing == "yes" or keep_playing == "Yes":
-        active = True
-    elif keep_playing == "n" or keep_playing == "N" or keep_playing == "no" or keep_playing == "No":
-        active = False
-    else:
-        print("That isn't an answer!")
-    
-    
+        keep_playing = input("Do you want to play? (y/n): ")
+        if keep_playing == "y" or keep_playing == "Y" or keep_playing == "yes" or keep_playing == "Yes":
+            active = True
+        elif keep_playing == "n" or keep_playing == "N" or keep_playing == "no" or keep_playing == "No":
+            active = False
+        else:
+            print("That isn't an answer!")
+
+        player1.toss_coin()
+        player2.toss_coin() 
+
+main()  
