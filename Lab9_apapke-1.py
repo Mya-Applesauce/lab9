7d1a3617905d20062362a4711e842101c47d1704
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-30 20:22:48 -0400

    started to get things set up and implemented exit statement

--- Lab9_apapke-1.py
+++ Lab9_apapke-1.py
@@ -4,3 +4,27 @@ Ari Papke
 coin matching game
 03/29/26
 """
+
+from player import Player
+
+def main():
+    player1 = Player("Sonic")
+    player2 = Player("Tails")
+
+print(f"+++ Coin Match Madness! +++")
+
+active = True
+
+while active:
+    print(f"{main()} has {} coins.")
+    print(f"{main()} has {} coins.\n")
+    
+    keep_playing = input("Do you want to play? (y/n): ")
+    if keep_playing == "y" or keep_playing == "Y" or keep_playing == "yes" or keep_playing == "Yes":
+        active = True
+    elif keep_playing == "n" or keep_playing == "N" or keep_playing == "no" or keep_playing == "No":
+        active = False
+    else:
+        print("That isn't an answer!")
+    
+    
